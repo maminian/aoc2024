c383ed0a17929adc883069ef7700322db9df6f45
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2024-12-03 22:35:15 -0800

    dec 03 python

--- dec04/solution.py
+++ dec04/solution.py
@@ -7,7 +7,7 @@ with open('input_mini', 'r') as f:
     fo = np.array([list(f.strip('\n')) for f in fo])
 #
 
-_PRINT=True
+_PRINT=False
 
 def p(*s):
     if _PRINT:
@@ -18,8 +18,8 @@ def in_bounds(_i,_j,m,n):
 def extract(array,direction,pos):
     i,j = pos
     buff=''
-    while in_bounds(i,j,*fo.shape):
-        buff+=fo[i,j]
+    while in_bounds(i,j,*array.shape):
+        buff+=array[i,j]
         i+=d[0]
         j+=d[1]
     return buff
@@ -44,15 +44,40 @@ do = list(do)
 do.remove((0,0))
 
 count=0
-for _start in starts:
-    p("\n",_start,"\n=============")
-    for d in do:
-        # bypass running in direction parallel to bdry.
-        if (d[0]==0 and _start[1] in [0,fo.shape[1]-1]) or (d[1]==0 and _start[0] in [0,fo.shape[0]-1]):
-            continue
-        b=extract(fo, d, _start)
-        count += len(po.findall(b))
-        p(d,b,len(po.findall(b)),count)
 
+for i in range(fo.shape[0]):
+    for d in [(-1,1),(0,1),(1,1)]:
+        b = extract(fo, d, (i,0))
+        count += len(po.findall(b))
+        count += len(po.findall(b[::-1]))
+        p(d,b,len(po.findall(b)),count)
+for j in range(fo.shape[1]):
+    for d in [(1,0),(1,1)]:
+        if d==(1,1) and j==0:
+            continue
+        b = extract(fo, d, (0,j))
+        count += len(po.findall(b))
+        count += len(po.findall(b[::-1]))
+        p(d,b,len(po.findall(b)),count)
+for i in range(1,fo.shape[0]):
+    for d in [(1,-1)]:
+        b = extract(fo, d, (i, fo.shape[1]-1))
+        count += len(po.findall(b))
+        count += len(po.findall(b[::-1]))
+        p(d,b,len(po.findall(b)),count)
 print(count)
+
+# Part 2
+mo = re.compile('MAS')
+count2 = 0
+for i in range(1,fo.shape[0]-1):
+    for j in range(1,fo.shape[1]-1):
+        if fo[i,j]!='A': continue
+        
+        s1 = ''.join([fo[i+k,j+k] for k in range(-1,2)])
+        s2 = ''.join([fo[i-k,j+k] for k in range(-1,2)])
+        if (mo.match(s1) or mo.match(s1[::-1])) and (mo.match(s2) or mo.match(s2[::-1])):
+            count2 += 1
+print(count2)
+
 
